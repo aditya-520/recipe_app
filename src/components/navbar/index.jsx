ab592555c9d4a278db05257800324494f0d5a0b1
kirtis20 <146112395+kirtis20@users.noreply.github.com>
2024-07-01 22:09:33 +0530

    NavBar (#5)
    * changed app link colour
    
    * added navbar
    
    * Added Context
    
    ---------
    
    Co-authored-by: Aditya <adityajk520@gmail.com>

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,9 +1,30 @@
+import React, { useContext } from 'react';
+import { GlobalContext } from '../../context';
+import './navbar.css';
 
+const Navbar = () => {
 
+    const { searchParam, setSearchParam } = useContext(GlobalContext)
+    console.log(searchParam);
 
-export default function Navbar(){
+    return (
+        <nav className="navbar">
+            <div className="navbar-logo">
+                <img src="/path_to_your_logo" alt="Logo" />
+            </div>
+            <div className="navbar-search">
+                <input type="text" 
+                value={searchParam}
+                onChange={(event)=> setSearchParam(event.target.value)}
+                placeholder="Search..." />
+            </div>
+            <div className="navbar-options">
+                <a href="#option1">Option 1</a>
+                <a href="#option2">Option 2</a>
 
-    return <nav className="Navbar">
-        Navbar
-    </nav>
+            </div>
+        </nav>
+    );
 }
+
+export default Navbar;
--- src/index.js
+++ src/index.js
@@ -4,13 +4,17 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import GlobalState from './context';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
     <React.StrictMode>
-    <App />
-  </React.StrictMode>
+      <GlobalState>
+        <App />
+      </GlobalState>
+
+    </React.StrictMode>
   </BrowserRouter>
 
 );
