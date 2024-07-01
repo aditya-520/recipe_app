05bee33f2d56d4fd43cdd0def318124f1a96cc32
kirtis20 <kirtis2005@gmail.com>
2024-07-01 21:33:52 +0530

    added navbar

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,9 +1,22 @@
+import React from 'react';
+import './navbar.css';
 
-
-
-export default function Navbar(){
-
-    return <nav className="Navbar">
-        Navbar
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="navbar-logo">
+        <img src="/path_to_your_logo" alt="Logo" />
+      </div>
+      <div className="navbar-search">
+        <input type="text" placeholder="Search..." />
+      </div>
+      <div className="navbar-options">
+        <a href="#option1">Option 1</a>
+        <a href="#option2">Option 2</a>
+        
+      </div>
     </nav>
+  );
 }
+
+export default Navbar;
