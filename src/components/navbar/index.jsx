933048c90280c75babb2fd34d8fd3204915286c5
Aditya <adityajk520@gmail.com>
2024-07-01 21:58:41 +0530

    Added Context

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,22 +1,30 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { GlobalContext } from '../../context';
 import './navbar.css';
 
 const Navbar = () => {
-  return (
-    <nav className="navbar">
-      <div className="navbar-logo">
-        <img src="/path_to_your_logo" alt="Logo" />
-      </div>
-      <div className="navbar-search">
-        <input type="text" placeholder="Search..." />
-      </div>
-      <div className="navbar-options">
-        <a href="#option1">Option 1</a>
-        <a href="#option2">Option 2</a>
-        
-      </div>
-    </nav>
-  );
+
+    const { searchParam, setSearchParam } = useContext(GlobalContext)
+    console.log(searchParam);
+
+    return (
+        <nav className="navbar">
+            <div className="navbar-logo">
+                <img src="/path_to_your_logo" alt="Logo" />
+            </div>
+            <div className="navbar-search">
+                <input type="text" 
+                value={searchParam}
+                onChange={(event)=> setSearchParam(event.target.value)}
+                placeholder="Search..." />
+            </div>
+            <div className="navbar-options">
+                <a href="#option1">Option 1</a>
+                <a href="#option2">Option 2</a>
+
+            </div>
+        </nav>
+    );
 }
 
 export default Navbar;
