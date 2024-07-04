60aae35d8894f47e90b981b266d33cc9149dbe69
Aditya <adityajk520@gmail.com>
2024-07-04 17:29:48 +0530

    recipe details api

--- src/components/recipe-item/index.jsx
+++ src/components/recipe-item/index.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import './recipeitem.css';
+import { Link } from 'react-router-dom';
 
 export default function RecipeItem({ item }) {
     return (
         <div className="recipe-item">
             <div className="image-container">
                 <img src={item?.image_url} alt="recipe item" className="image-block" />
+            </div>
+            <div>
+                <span classname="item-pub">{item?.publisher}</span>
+                <h3 classname="item-name">{item?.title}</h3>
+                <Link to={`/recipe-item/${item?.id}`} className='abcd'>Recipe Details</Link>
             </div>
         </div>
     );
--- src/pages/details/index.jsx
+++ src/pages/details/index.jsx
@@ -1,7 +1,34 @@
+import { useContext, useEffect } from "react";
+import { useParams } from "react-router-dom"
+import { GlobalContext } from "../../context";
 
 
 
-export default function Details(){
+export default function Details() {
+
+    const { id } = useParams();
+    const {
+        recipeDetailsData,
+        setRecipeDetailsData,
+    } = useContext(GlobalContext);
+
+    useEffect(() => {
+        async function getRecipeDetails() {
+            const response = await fetch(
+                `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
+            );
+            const data = await response.json();
+
+            console.log(data);
+            if (data?.data) {
+                setRecipeDetailsData(data?.data);
+            }
+        }
+
+        getRecipeDetails();
+    }, []);
+
+    console.log(recipeDetailsData, "recipeDetailsData");
 
     return <div>Details</div>
 }
