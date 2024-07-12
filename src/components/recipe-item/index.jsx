88a63fad4ff590197457b590a824197f782cfd24
aditya-520 <122147290+aditya-520@users.noreply.github.com>
2024-07-12 21:11:25 +0530

    Kirti (#12)
    * changed app link colour
    
    * added navbar
    
    * Added Context
    
    * included api
    
    * images of recipe-item
    
    * changed something
    
    * added css to home + recipe item
    
    * recipe details api
    
    * details page changed
    
    * navbar+recipe item + details
    
    * added ingrediant details
    
    * details page me kuch add kiya
    
    * ultimate changes
    
    ---------
    
    Co-authored-by: kirtis20 <kirtis2005@gmail.com>
    Co-authored-by: kirtis20 <146112395+kirtis20@users.noreply.github.com>

--- src/components/recipe-item/index.jsx
+++ src/components/recipe-item/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import './recipeitem.css';
 import { Link } from 'react-router-dom';
+import './recipeitem.css';
 
 export default function RecipeItem({ item }) {
     return (
@@ -8,12 +8,11 @@ export default function RecipeItem({ item }) {
             <div className="image-container">
                 <img src={item?.image_url} alt="recipe item" className="image-block" />
             </div>
-            <div>
-                <span classname="item-pub">{item?.publisher}</span>
-                <h3 classname="item-name">{item?.title}</h3>
-                <Link to={`/recipe-item/${item?.id}`} className='abcd'>Recipe Details</Link>
+            <div className="recipe-details">
+                <span className="item-pub">{item?.publisher}</span>
+                <h3 className="item-name">{item?.title}</h3>
+                <Link to={`/recipe-item/${item?.id}`} className='recipe-button'>Recipe Details</Link>
             </div>
         </div>
     );
 }
-
--- src/pages/details/index.jsx
+++ src/pages/details/index.jsx
@@ -1,16 +1,12 @@
 import { useContext, useEffect } from "react";
-import { useParams } from "react-router-dom"
+import { useParams } from "react-router-dom";
 import { GlobalContext } from "../../context";
-
-
+import "./details.css";
 
 export default function Details() {
-
     const { id } = useParams();
-    const {
-        recipeDetailsData,
-        setRecipeDetailsData,
-    } = useContext(GlobalContext);
+    const { recipeDetailsData, setRecipeDetailsData,favouritesList, handleAddToFavourite } =
+        useContext(GlobalContext);
 
     useEffect(() => {
         async function getRecipeDetails() {
@@ -21,14 +17,56 @@ export default function Details() {
 
             console.log(data);
             if (data?.data) {
-                setRecipeDetailsData(data?.data);
+                setRecipeDetailsData(data.data.recipe);
             }
         }
 
         getRecipeDetails();
-    }, []);
+    }, [id,setRecipeDetailsData]);
 
     console.log(recipeDetailsData, "recipeDetailsData");
 
-    return <div>Details</div>
+    return (
+        <div className="details-container">
+            <div className="image-container">
+                <img
+                    src={recipeDetailsData?.image_url}
+                    alt={recipeDetailsData?.title}
+                    className="recipe-image"
+                />
+            </div>
+            <div className="details-content">
+                <span className="publisher">{recipeDetailsData?.recipe?.publisher}</span>
+                <h3 className="recipe-title">{recipeDetailsData?.recipe?.title}</h3>
+                <button
+                    onClick={() => handleAddToFavourite(recipeDetailsData?.recipe)}
+                    className="favbutton"
+                >
+                    {favouritesList &&
+                        favouritesList.length > 0 &&
+                        favouritesList.findIndex(
+                            (item) => item.id === recipeDetailsData?.recipe?.id
+                        ) !== -1
+                        ? "Remove from favourites"
+                        : "Add to favourites"}
+                </button>
+                <div className="ingredients-section">
+                    <span className="ingredients-title">Ingredients:</span>
+                    <ul className="ingredients-list">
+                        {recipeDetailsData?.ingredients.map((ingredient, index) => (
+                            <li key={index} className="ingredient-item">
+                                <span className="ingredient-quantity">
+                                    {ingredient.quantity || ""}
+                                </span>
+                                <span className="ingredient-unit">{ingredient.unit || ""}</span>
+                                <span className="ingredient-description">
+                                    {ingredient.description}
+                                </span>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            </div>
+        </div>
+    );
 }
